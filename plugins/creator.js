861f6903b66c59fe9c2be6a68a4b592c53a2e21c
DARK UMAR <103824676+umarlegend@users.noreply.github.com>
2022-10-28 19:19:21 +0500

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,8 @@
 function handler(m) {
   
   const kontak = {
-	"displayName": 'My Owner Umar',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('923165123719@s.whatsapp.net')}\nitem1.TEL;waid=923165123719:923165123719\nitem1.X-ABLabel:save-me.\nURL;My Web: https://github.com/umar404\nEMAIL;Email Owner:umardevil48@gamil.com\nORG: NOT A BOT +SAVE ME\nTEL;My number ;waid=48888781888:48888781888\nEND:VCARD`
+	"displayName": 'My Owner Sanwal',
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('966594204996@s.whatsapp.net')}\nitem1.TEL;waid=966594204996:966594204996\nitem1.X-ABLabel:save-me.\nURL;My Web: https://github.com/umar404/ud-md\nEMAIL;Email Owner:umardevil48@gamil.com\nORG: NOT A BOT +SAVE ME\nTEL;My number ;waid=923165123719:923165123719\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
